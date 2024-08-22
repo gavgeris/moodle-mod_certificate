705aa5d0ab147d1d0e0e89285409a86ce2bc1171
george <gavgeris@gmail.com>
2024-08-22 22:32:57 +0300

    Add get_course_metadata function

--- lib.php
+++ lib.php
@@ -378,5 +378,19 @@ function certificate_get_user_role($course, $USER) {
 		//die(0);
 		return $row->archetype;
 }
-
+function get_course_metadata($courseid) {
+    $handler = \core_customfield\handler::get_handler('core_course', 'course');
+    // This is equivalent to the line above.
+    //$handler = \core_course\customfield\course_handler::create();
+    $datas = $handler->get_instance_data($courseid);
+    $metadata = [];
+    foreach ($datas as $data) {
+        if (empty($data->get_value())) {
+            continue;
+        }
+        $cat = $data->get_field()->get_category()->get('name');
+        $metadata[$data->get_field()->get('shortname')] = $cat . ': ' . $data->get_value();
+    }
+    return $metadata;
+}
 // ============================================================================
